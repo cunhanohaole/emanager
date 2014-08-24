9e9db04ba61df6d0b92485ccbe2fee551d460249
Fernando Alves <cunhanohaole@gmail.com>
2014-08-24 22:32:08 +1000

    fixing log location for production and database scripts

--- email-manager-database/scripts/versions/3/3.0.x/upgrade.sql
+++ email-manager-database/scripts/versions/3/3.0.x/upgrade.sql
@@ -1,17 +1,17 @@
 -- Drops column sender_config from user
-ALTER TABLE `cunha_emanager`.`user`
+ALTER TABLE `user`
 DROP FOREIGN KEY `fk_user_sender_config1`
 
-ALTER TABLE `cunha_emanager`.`user`
+ALTER TABLE `user`
 DROP COLUMN `sender_config_id`,
 DROP INDEX `fk_user_sender_config1_idx`
 
 --Adds FK to user.username in sender_config
-ALTER TABLE `cunha_emanager`.`sender_config`
+ALTER TABLE `sender_config`
 ADD COLUMN `user_username` VARCHAR(45) NOT NULL AFTER `id`,
 ADD INDEX `fk_sender_config_user1_idx` (`user_username` ASC);
 
-ALTER TABLE `cunha_emanager`.`sender_config`
+ALTER TABLE `sender_config`
 ADD CONSTRAINT `fk_sender_config_user1`
   FOREIGN KEY (`user_username`)
   REFERENCES `cunha_emanager`.`user` (`username`)
@@ -19,11 +19,11 @@ ADD CONSTRAINT `fk_sender_config_user1`
   ON UPDATE NO ACTION;
 
 -- Add FK to SenderConfig on Email so that we can identify who sent the email
-ALTER TABLE `cunha_emanager`.`email`
+ALTER TABLE `email`
 ADD COLUMN `sender_config_id` INT(11) NOT NULL AFTER `status`,
 ADD INDEX `fk_email_sender_config1_idx` (`sender_config_id` ASC);
 
-ALTER TABLE `cunha_emanager`.`email`
+ALTER TABLE `email`
 ADD CONSTRAINT `fk_email_sender_config1`
   FOREIGN KEY (`sender_config_id`)
   REFERENCES `cunha_emanager`.`sender_config` (`id`)
